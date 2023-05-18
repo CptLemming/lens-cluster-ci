07617602914455c5f49ab89002e5701d8d8338a5
Ashley Wilson <scifilem@gmail.com>
2023-05-18 08:35:00 +0100

    feat: Add tab for pods list

--- src/example-page/example-page.tsx
+++ src/example-page/example-page.tsx
@@ -3,11 +3,7 @@ import path from "path";
 import React from "react";
 
 const {
-  K8sApi: {
-    configMapApi,
-    nodesApi,
-    deploymentApi,
-  },
+  K8sApi: { configMapApi, nodesApi, deploymentApi, podsApi },
   Component: {
     Checkbox,
     Input,
@@ -16,117 +12,186 @@ const {
     TableCell,
     TableHead,
     TabLayout,
-  }
+    Tabs,
+    Tab,
+  },
 } = Renderer;
 
 type ConfigMap = Renderer.K8sApi.ConfigMap;
 type Node = Renderer.K8sApi.Node;
 type Deployment = Renderer.K8sApi.Deployment;
+type Pod = Renderer.K8sApi.Pod;
 
 export function ExampleIcon(props: Renderer.Component.IconProps) {
-  return <Renderer.Component.Icon {...props} material="support_agent" tooltip={path.basename(__filename)}/>
+  return (
+    <Renderer.Component.Icon
+      {...props}
+      material="support_agent"
+      tooltip={path.basename(__filename)}
+    />
+  );
 }
 
 type State = {
   config: ConfigMap | null;
   deployment: Deployment | null;
   nodes: Record<string, Node>;
-}
+  pods: Record<string, Pod>;
+  activeTab: number;
+};
 
-export class ExamplePage extends React.Component<{ extension: Renderer.LensExtension }, State> {
-
+export class ExamplePage extends React.Component<
+  { extension: Renderer.LensExtension },
+  State
+> {
   state: State = {
     config: null,
     deployment: null,
     nodes: {},
-  }
+    pods: {},
+    activeTab: 0,
+  };
 
   private configWatcher: any;
   private deploymentWatcher: any;
   private nodesWatcher: any;
+  private podsWatcher: any;
 
   async componentDidMount() {
     // TODO Manual fetch and watch is wasteful. Investigate catalogs
-    const config = await configMapApi.get({ name: "ci-resources", namespace: "kube-system" });
-    this.setState(prev => ({
+    const config = await configMapApi.get({
+      name: "ci-resources",
+      namespace: "kube-system",
+    });
+    this.setState((prev) => ({
       ...prev,
       config,
     }));
 
-    this.configWatcher = configMapApi.watch({ namespace: "kube-system", callback: (data) => {
-      console.log("config map data", data);
-      if (data) {
-        const config = (data.object as any) as ConfigMap;
-        if (config.metadata.name === "ci-resources") {
-          this.setState(prev => ({
-            ...prev,
-            config,
-          }));
+    this.configWatcher = configMapApi.watch({
+      namespace: "kube-system",
+      callback: (data) => {
+        console.log("config map data", data);
+        if (data) {
+          const config = data.object as any as ConfigMap;
+          if (config.metadata.name === "ci-resources") {
+            this.setState((prev) => ({
+              ...prev,
+              config,
+            }));
+          }
         }
-      }
-    }});
+      },
+    });
 
     const nodesList = await nodesApi.list();
-    const nodes = nodesList.reduce((collection, node) => ({
-      ...collection,
-      [node.metadata.name]: node,
-    }), {});
-    this.setState(prev => ({
+    const nodes = nodesList.reduce(
+      (collection, node) => ({
+        ...collection,
+        [node.metadata.name]: node,
+      }),
+      {}
+    );
+    this.setState((prev) => ({
       ...prev,
       nodes,
     }));
 
-    this.nodesWatcher = nodesApi.watch({ callback: (data) => {
-      console.log("nodes data", data);
-      if (data) {
-        const node = (data.object as any) as Node;
-        this.setState((prev) => ({
-          ...prev,
-          nodes: {
-            ...prev.nodes,
-            [node.metadata.name]: node,
-          } as any,
-        }));
-      }
-    }});
+    this.nodesWatcher = nodesApi.watch({
+      callback: (data) => {
+        console.log("nodes data", data);
+        if (data) {
+          const node = data.object as any as Node;
+          this.setState((prev) => ({
+            ...prev,
+            nodes: {
+              ...prev.nodes,
+              [node.metadata.name]: node,
+            } as any,
+          }));
+        }
+      },
+    });
 
-    const deployment = await deploymentApi.get({ name: "kube0", namespace: "buildkit" });
-    this.setState(prev => ({
+    const podsList = await podsApi.list({ namespace: "jenkins" });
+    const pods = podsList.reduce(
+      (collection, pod) => ({
+        ...collection,
+        [pod.metadata.name]: pod,
+      }),
+      {}
+    );
+    this.setState((prev) => ({
+      ...prev,
+      pods,
+    }));
+
+    this.podsWatcher = podsApi.watch({
+      namespace: "jenkins",
+      callback: (data) => {
+        console.log("pods data", data);
+        if (data) {
+          const pod = data.object as any as Pod;
+          this.setState((prev) => ({
+            ...prev,
+            pods: {
+              ...prev.nodes,
+              [pod.metadata.name]: pod,
+            } as any,
+          }));
+        }
+      },
+    });
+
+    const deployment = await deploymentApi.get({
+      name: "kube0",
+      namespace: "buildkit",
+    });
+    this.setState((prev) => ({
       ...prev,
       deployment,
     }));
 
-    this.deploymentWatcher = deploymentApi.watch({ namespace: "buildkit", callback: (data) => {
-      console.log("deployment data", data);
-      if (data) {
-        const deployment = (data.object as any) as Deployment;
-        if (deployment.metadata.name === "kube0") {
-          this.setState(prev => ({
-            ...prev,
-            deployment,
-          }));
+    this.deploymentWatcher = deploymentApi.watch({
+      namespace: "buildkit",
+      callback: (data) => {
+        console.log("deployment data", data);
+        if (data) {
+          const deployment = data.object as any as Deployment;
+          if (deployment.metadata.name === "kube0") {
+            this.setState((prev) => ({
+              ...prev,
+              deployment,
+            }));
+          }
         }
-      }
-    }});
+      },
+    });
   }
 
   async componentWillUnmount() {
     if (this.configWatcher) this.configWatcher();
     if (this.deploymentWatcher) this.deploymentWatcher();
     if (this.nodesWatcher) this.nodesWatcher();
+    if (this.podsWatcher) this.podsWatcher();
   }
 
   async onToggleNodeLabel(node: Node, label: string, addLabel: boolean) {
-    console.log(`Toggle node ${node.metadata.name} label ${label} to ${addLabel}`);
+    console.log(
+      `Toggle node ${node.metadata.name} label ${label} to ${addLabel}`
+    );
 
-    const result = await nodesApi.patch({ name: node.metadata.name }, {
-      metadata: {
-        labels: {
-          ...node.metadata.labels,
-          [label]: addLabel ? "" : null,
+    const result = await nodesApi.patch(
+      { name: node.metadata.name },
+      {
+        metadata: {
+          labels: {
+            ...node.metadata.labels,
+            [label]: addLabel ? "" : null,
+          },
         },
       }
-    });
+    );
 
     console.log(result);
   }
@@ -134,11 +199,14 @@ export class ExamplePage extends React.Component<{ extension: Renderer.LensExten
   async onUpdateConfig(field: string, value: string) {
     console.log(`Update config ${field} to ${value}`);
 
-    const result = await configMapApi.patch({ name: "ci-resources", namespace: "kube-system" }, {
-      data: {
-        [field]: value,
-      },
-    });
+    const result = await configMapApi.patch(
+      { name: "ci-resources", namespace: "kube-system" },
+      {
+        data: {
+          [field]: value,
+        },
+      }
+    );
 
     console.log(result);
   }
@@ -146,11 +214,14 @@ export class ExamplePage extends React.Component<{ extension: Renderer.LensExten
   async onUpdateBuildKitDeployments(replicas: string) {
     console.log(`Update buildkit deployment ${replicas}`);
 
-    const result = await deploymentApi.patch({ name: "kube0", namespace: "buildkit" }, {
-      spec: {
-        replicas: parseInt(replicas),
-      },
-    });
+    const result = await deploymentApi.patch(
+      { name: "kube0", namespace: "buildkit" },
+      {
+        spec: {
+          replicas: parseInt(replicas),
+        },
+      }
+    );
 
     console.log(result);
   }
@@ -158,31 +229,57 @@ export class ExamplePage extends React.Component<{ extension: Renderer.LensExten
   async onUpdateJenkinsResources(node: Node, resources: string) {
     console.log(`Update node ${node.metadata.name} resources ${resources}`);
 
-    const result = await nodesApi.patch({ name: node.metadata.name }, {
-      metadata: {
-        labels: {
-          ...node.metadata.labels,
-          "scheduler/jenkins": resources,
-        }
+    const result = await nodesApi.patch(
+      { name: node.metadata.name },
+      {
+        metadata: {
+          labels: {
+            ...node.metadata.labels,
+            "scheduler/jenkins": resources,
+          },
+        },
       }
-    });
+    );
 
     console.log(result);
   }
 
   render() {
+    const { activeTab } = this.state;
+    
     const nodes = Object.values(this.state.nodes);
-    const available = nodes.reduce((sum, node) => sum + parseInt(node.metadata.labels?.["scheduler/jenkins"]), 0);
+    const pods = Object.values(this.state.pods);
+    const e2eResource = this.state.config?.data?.["e2e-resource"];
+    const prResources = this.state.config?.data?.["pr-resource"];
+
+    const available = nodes.reduce(
+      (sum, node) =>
+        sum + parseInt(node.metadata.labels?.["scheduler/jenkins"]),
+      0
+    );
+    const used = pods.reduce((sum, pod) => {
+      const isE2E = pod.metadata.name.includes("e2e");
+      return sum + (isE2E ? parseInt(e2eResource) : parseInt(prResources));
+    }, 0);
 
     return (
       <TabLayout>
         <div className="flex column gaps" style={{ height: "100%" }}>
-          <div style={{ background: "var(--contentColor)", padding: "calc(var(--margin)*2)" }}>
+          <div
+            style={{
+              background: "var(--contentColor)",
+              padding: "calc(var(--margin)*2)",
+            }}
+          >
             <h4>Cluster resources</h4>
 
             <div>
-              <p>Available: <i>{available}</i></p>
-              <p>Used: <i>TODO</i></p>
+              <p>
+                Available: <i>{available}</i>
+              </p>
+              <p>
+                Used: <i>{used}</i>
+              </p>
             </div>
           </div>
 
@@ -190,14 +287,20 @@ export class ExamplePage extends React.Component<{ extension: Renderer.LensExten
             <div>{JSON.stringify(this.deployment)}</div>
           </div> */}
 
-          <div className="flex column gaps" style={{ background: "var(--contentColor)", padding: "calc(var(--margin)*2)" }}>
+          <div
+            className="flex column gaps"
+            style={{
+              background: "var(--contentColor)",
+              padding: "calc(var(--margin)*2)",
+            }}
+          >
             <h5>Resources</h5>
 
             <div>
               <p>E2E</p>
               <Input
                 name="e2e-resource"
-                value={this.state.config?.data?.["e2e-resource"]}
+                value={e2eResource}
                 onChange={(value) => this.onUpdateConfig("e2e-resource", value)}
               />
             </div>
@@ -206,7 +309,7 @@ export class ExamplePage extends React.Component<{ extension: Renderer.LensExten
               <p>PR</p>
               <Input
                 name="pr-resource"
-                value={this.state.config?.data?.["pr-resource"]}
+                value={prResources}
                 onChange={(value) => this.onUpdateConfig("pr-resource", value)}
               />
             </div>
@@ -221,46 +324,87 @@ export class ExamplePage extends React.Component<{ extension: Renderer.LensExten
             </div>
           </div>
 
-          <div className="flex column gaps" style={{ background: "var(--contentColor)", padding: "calc(var(--margin)*2)", flex: 1 }}>
-            <h5>Nodes</h5>
+          <div
+            className="flex column gaps"
+            style={{
+              background: "var(--contentColor)",
+              padding: "calc(var(--margin)*2)",
+              flex: 1,
+            }}
+          >
+            <Tabs
+              scrollable
+              value={activeTab}
+              onChange={(tabIndex) => this.setState({ activeTab: tabIndex })}
+            >
+              <Tab label="Nodes" value={0} />
+              <Tab label="Pods" value={1} />
+            </Tabs>
 
-            <Table>
-              <TableHead>
-                <TableCell>Node</TableCell>
-                <TableCell>Resources</TableCell>
-                <TableCell>BuildKit</TableCell>
-                <TableCell>Jenkins worker</TableCell>
-              </TableHead>
+            {activeTab === 0 && (
+              <Table>
+                <TableHead>
+                  <TableCell>Node</TableCell>
+                  <TableCell>Resources</TableCell>
+                  <TableCell>BuildKit</TableCell>
+                  <TableCell>Jenkins worker</TableCell>
+                </TableHead>
 
-              {Object.values(this.state.nodes).map(node => (
-                <TableRow nowrap>
-                  <TableCell>{node.metadata.name}</TableCell>
-                  <TableCell>
-                    <Input
-                      value={node.metadata.labels?.["scheduler/jenkins"]}
-                      onChange={(val) => this.onUpdateJenkinsResources(node, val)}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <Checkbox
-                      label="Enabled"
-                      value={("docker/buildkit" in node.metadata.labels)}
-                      onChange={(val) => this.onToggleNodeLabel(node, "docker/buildkit", val)}
-                    />
-                  </TableCell>
-                  <TableCell>
-                    <Checkbox
-                      label="Enabled"
-                      value={("jenkins/worker" in node.metadata.labels)}
-                      onChange={(val) => this.onToggleNodeLabel(node, "jenkins/worker", val)}
-                    />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </Table>
+                {nodes.map((node) => (
+                  <TableRow nowrap>
+                    <TableCell>{node.metadata.name}</TableCell>
+                    <TableCell>
+                      <Input
+                        value={node.metadata.labels?.["scheduler/jenkins"]}
+                        onChange={(val) =>
+                          this.onUpdateJenkinsResources(node, val)
+                        }
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        label="Enabled"
+                        value={"docker/buildkit" in node.metadata.labels}
+                        onChange={(val) =>
+                          this.onToggleNodeLabel(node, "docker/buildkit", val)
+                        }
+                      />
+                    </TableCell>
+                    <TableCell>
+                      <Checkbox
+                        label="Enabled"
+                        value={"jenkins/worker" in node.metadata.labels}
+                        onChange={(val) =>
+                          this.onToggleNodeLabel(node, "jenkins/worker", val)
+                        }
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </Table>
+            )}
+            {activeTab === 1 && (
+              <Table>
+                <TableHead>
+                  <TableCell>Pod</TableCell>
+                  <TableCell>Resources</TableCell>
+                  <TableCell>Node</TableCell>
+                </TableHead>
+
+                {pods.map((pod) => (
+                  <TableRow nowrap>
+                    <TableCell>{pod.metadata.name}</TableCell>
+                    <TableCell>
+                      <i>TODO</i>
+                    </TableCell>
+                    <TableCell>{pod.spec.nodeName}</TableCell>
+                  </TableRow>
+                ))}
+              </Table>
+            )}
           </div>
         </div>
       </TabLayout>
-    )
+    );
   }
 }
